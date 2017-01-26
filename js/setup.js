a089eb6c6b34c9b6add383849f59e4606483bbf2
Marina Rusakova <marinarusakova111@gmail.com>
2017-01-26 15:52:15 -0800

    Выполнила задание c изменением параметров цвета 2

---
+++ js/setup.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var initWizardCustomizationWindow = function() {
+
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+
+  setupOpen.addEventListener('click', function () {
+    setup.classList.remove('invisible');
+  });
+
+  setupClose.addEventListener('click', function () {
+    setup.classList.add('invisible');
+  });
+
+  var wizard = document.querySelector('#wizard');
+  var wizardCoat = wizard.querySelector('#wizard-coat');
+  var wizardCoatColors = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+
+  wizardCoat.addEventListener('click', function () {
+    var colorNumber = Math.floor(Math.random() * wizardCoatColors.length);
+    wizardCoat.style.fill = wizardCoatColors[colorNumber]; 
+  });
+
+  var wizardEyes = wizard.querySelector('#wizard-eyes');
+  var wizardEyesColors = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+
+  wizardEyes.addEventListener('click', function () {
+    var colorNumber = Math.floor(Math.random() * wizardEyesColors.length);
+    wizardEyes.style.fill = wizardEyesColors[colorNumber]; 
+  });
+
+  var fireball = document.querySelector('.setup-fireball-wrap');
+  var fireballColors = [
+    '#ee4830',
+    '#30a8ee',
+    '#5ce6c0',
+    '#e848d5',
+    '#e6e848'
+  ];
+
+  fireball.addEventListener('click', function () {
+    var colorNumber = Math.floor(Math.random() * fireballColors.length);
+    fireball.style.background = fireballColors[colorNumber]; 
+  });
+};
+
+
+initWizardCustomizationWindow();
