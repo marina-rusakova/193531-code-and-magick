645468711d2ad612f76eeb8c74994652065b13ee
Marina Rusakova <marinarusakova111@gmail.com>
2017-02-10 01:13:33 -0800

    Выполнила задание с модуляцией цвета

---
+++ js/colorize-element.js
@@ -0,0 +1,34 @@
+'use strict';
+
+window.colorizeElement = function(element, colors, property) {
+  var currentColor = colors[0];
+  var ENTER_KEY_CODE = 13;
+
+  var isEnterPressed = function (evt) {
+    return evt.keyCode && evt.keyCode === ENTER_KEY_CODE;
+  };
+
+  element.addEventListener('click', function () {
+    var newColor = window.utils.getRandomElementExcept(colors, currentColor);
+
+    currentColor = newColor;
+    element.style[property]= currentColor;
+  });
+
+  element.addEventListener('keyup', function (evt) {
+    if (isEnterPressed(evt)) {
+      element.setAttribute('aria-pressed', false);
+
+      var newColor = window.utils.getRandomElementExcept(colors, currentColor);
+
+      currentColor = newColor;
+      element.style[property]= currentColor;
+    }
+  });
+
+  element.addEventListener('keydown', function (evt) {
+    if (isEnterPressed(evt)) {
+      element.setAttribute('aria-pressed', true);
+    }
+  });
+};
---
+++ js/utils.js
@@ -0,0 +1,25 @@
+'use strict';
+
+window.utils = (function() {
+  var getRandomElement = function(array) {
+    var randomElementIndex =  Math.floor(Math.random() * array.length);
+    return array[randomElementIndex];
+  };
+
+  var getRandomElementExcept = function(colors, currentColor) {
+    var newColor = null;
+
+    while (!newColor || newColor === currentColor) {
+      newColor = getRandomElement(colors);
+    }
+
+    return newColor;
+  };
+
+  return {
+    getRandomElementExcept: getRandomElementExcept
+
+  };
+})();
+
+
